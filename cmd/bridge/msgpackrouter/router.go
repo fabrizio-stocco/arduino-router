6d22cbe47cdf5dafadf657cad4870c2a500b89fb
Cristian Maglie <c.maglie@arduino.cc>
2025-04-14 17:53:27 +0200

    Added msgpackrouter

---
+++ cmd/bridge/msgpackrouter/router.go
@@ -0,0 +1,123 @@
+package msgpackrouter
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log/slog"
+	"maps"
+	"sync"
+
+	"github.com/arduino/bridge/msgpackrpc"
+)
+
+type Router struct {
+	routesLock sync.Mutex
+	routes     map[string]*msgpackrpc.Connection
+}
+
+func New() Router {
+	return Router{
+		routes: make(map[string]*msgpackrpc.Connection),
+	}
+}
+
+func (r *Router) Accept(conn io.ReadWriteCloser) {
+	go r.connectionLoop(conn)
+}
+
+func (r *Router) connectionLoop(conn io.ReadWriteCloser) {
+	defer conn.Close()
+
+	var msgpackconn *msgpackrpc.Connection
+	msgpackconn = msgpackrpc.NewConnection(conn, conn,
+		func(ctx context.Context, _ msgpackrpc.FunctionLogger, method string, params []any, respCallback func(result any, err any)) {
+			// This handler is called when a request is received from the client
+
+			// Check if the client is trying to register a new method
+			if method == "$/register" {
+				if len(params) != 1 {
+					respCallback(nil, fmt.Sprintf("invalid params: only one param is expected, got %d", len(params)))
+				} else if methodToRegister, ok := params[0].(string); !ok {
+					respCallback(nil, fmt.Sprintf("invalid params: expected string, got %T", params[0]))
+				} else if err := r.registerMethod(methodToRegister, msgpackconn); err != nil {
+					respCallback(nil, err.Error())
+				} else {
+					respCallback(nil, nil)
+				}
+				return
+			}
+
+			// Check if the method is registered
+			client, ok := r.getConnectionForMethod(method)
+			if !ok {
+				respCallback(nil, fmt.Sprintf("method %s not available", method))
+				return
+			}
+
+			// Forward the call to the registered client
+			reqResult, reqError, err := client.SendRequest(ctx, method, params)
+			if err != nil {
+				slog.Error("Failed to send request", "method", method, "err", err)
+				respCallback(nil, fmt.Sprintf("failed to send request: %s", err))
+				return
+			}
+
+			// Send the response back to the original caller
+			respCallback(reqResult, reqError)
+		},
+		func(_ msgpackrpc.FunctionLogger, method string, params []any) {
+			// This handler is called when a notification is received from the client
+
+			// Check if the method is registered
+			client, ok := r.getConnectionForMethod(method)
+			if !ok {
+				// if the method is not registered, the notifitication is lost
+				return
+			}
+
+			// Forward the notification to the registered client
+			if err := client.SendNotification(method, params); err != nil {
+				slog.Error("Failed to send notification", "method", method, "err", err)
+				return
+			}
+		},
+		func(err error) {
+			slog.Error("Error in connection", "err", err)
+		},
+	)
+
+	msgpackconn.Run()
+
+	// Unregister the methods when the connection is terminated
+	r.removeMethodsFromConnection(msgpackconn)
+	msgpackconn.Close()
+
+}
+
+func (r *Router) registerMethod(method string, conn *msgpackrpc.Connection) error {
+	r.routesLock.Lock()
+	defer r.routesLock.Unlock()
+
+	if _, ok := r.routes[method]; ok {
+		return fmt.Errorf("route already exists: %s", method)
+	}
+	r.routes[method] = conn
+	return nil
+}
+
+func (r *Router) removeMethodsFromConnection(conn *msgpackrpc.Connection) {
+	r.routesLock.Lock()
+	defer r.routesLock.Unlock()
+
+	maps.DeleteFunc(r.routes, func(k string, v *msgpackrpc.Connection) bool {
+		return v == conn
+	})
+}
+
+func (r *Router) getConnectionForMethod(method string) (*msgpackrpc.Connection, bool) {
+	r.routesLock.Lock()
+	defer r.routesLock.Unlock()
+	conn, ok := r.routes[method]
+	return conn, ok
+}
---
+++ cmd/bridge/msgpackrouter/router_test.go
@@ -0,0 +1,125 @@
+package msgpackrouter_test
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/arduino/bridge/msgpackrouter"
+	"github.com/arduino/bridge/msgpackrpc"
+	"github.com/djherbis/buffer"
+	"github.com/djherbis/nio/v3"
+	"github.com/stretchr/testify/require"
+)
+
+type FullPipe struct {
+	in  *nio.PipeReader
+	out *nio.PipeWriter
+}
+
+func (p *FullPipe) Read(b []byte) (int, error) {
+	return p.in.Read(b)
+}
+
+func (p *FullPipe) Write(b []byte) (int, error) {
+	return p.out.Write(b)
+}
+
+func (p *FullPipe) Close() error {
+	err1 := p.out.Close()
+	err2 := p.in.Close()
+	if err1 != nil {
+		return err1
+	}
+	if err2 != nil {
+		return err2
+	}
+	return nil
+}
+
+func newFullPipe() (io.ReadWriteCloser, io.ReadWriteCloser) {
+	in1, out1 := nio.Pipe(buffer.New(1024))
+	in2, out2 := nio.Pipe(buffer.New(1024))
+	return &FullPipe{in1, out2}, &FullPipe{in2, out1}
+}
+
+func TestBasicRouterFunctionality(t *testing.T) {
+	ch1a, ch1b := newFullPipe()
+	ch2a, ch2b := newFullPipe()
+
+	cl1Notifications := bytes.NewBuffer(nil)
+	cl1 := msgpackrpc.NewConnection(ch1a, ch1a, func(ctx context.Context, logger msgpackrpc.FunctionLogger, method string, params []any, respCallback func(result any, err any)) {
+		switch method {
+		case "ping":
+			respCallback(params, nil)
+		default:
+			respCallback(nil, "unknown method: "+method)
+		}
+	}, func(logger msgpackrpc.FunctionLogger, method string, params []any) {
+		fmt.Fprintf(cl1Notifications, "notification: %s %+v\n", method, params)
+	}, func(err error) {
+	})
+	go cl1.Run()
+
+	cl2 := msgpackrpc.NewConnection(ch2a, ch2a, func(ctx context.Context, logger msgpackrpc.FunctionLogger, method string, params []any, respCallback func(result any, err any)) {
+		respCallback(nil, nil)
+	}, func(logger msgpackrpc.FunctionLogger, method string, params []any) {
+	}, func(err error) {
+	})
+	go cl2.Run()
+
+	router := msgpackrouter.New()
+	router.Accept(ch1b)
+	router.Accept(ch2b)
+
+	{
+		// Register a method on the first client
+		result, reqErr, err := cl1.SendRequest(context.Background(), "$/register", []any{"ping"})
+		require.Nil(t, result)
+		require.Nil(t, reqErr)
+		require.NoError(t, err)
+	}
+	{
+		// Try to re-register the same method
+		result, reqErr, err := cl1.SendRequest(context.Background(), "$/register", []any{"ping"})
+		require.Nil(t, result)
+		require.Equal(t, "route already exists: ping", reqErr)
+		require.NoError(t, err)
+	}
+	{
+		// Register a method on the second client
+		result, reqErr, err := cl2.SendRequest(context.Background(), "$/register", []any{"temperature"})
+		require.Nil(t, result)
+		require.Nil(t, reqErr)
+		require.NoError(t, err)
+	}
+	{
+		// Call from client2 the registered method on client1
+		result, reqErr, err := cl2.SendRequest(context.Background(), "ping", []any{"1", int16(2), true})
+		require.Equal(t, []any{"1", int16(2), true}, result)
+		require.Nil(t, reqErr)
+		require.NoError(t, err)
+	}
+	{
+		// Call from client2 an un-registered method
+		result, reqErr, err := cl2.SendRequest(context.Background(), "not-existent-method", []any{"1", int16(2), true})
+		require.Nil(t, result)
+		require.Equal(t, "method not-existent-method not available", reqErr)
+		require.NoError(t, err)
+	}
+	{
+		// Send notification to client1
+		err := cl2.SendNotification("ping", []any{"a", int16(4), false})
+		require.NoError(t, err)
+	}
+	{
+		// Send notification to unregistered method
+		err := cl2.SendNotification("notexistent", []any{"a", int16(4), false})
+		require.NoError(t, err)
+	}
+	time.Sleep(100 * time.Millisecond) // Give some time for the notifications to be processed
+	require.Contains(t, cl1Notifications.String(), "notification: ping [a 4 false]")
+}
